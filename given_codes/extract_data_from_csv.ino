bac086a3164abc7ecfc02d60141cbada2d5d3695
Wiek Keiren <w.w.t.keiren@students.uu.nl>
2026-09-17 16:29:59 +0200

    fixed indents

--- given_codes/extract_data_from_csv.ino
+++ given_codes/extract_data_from_csv.ino
@@ -1,23 +1,28 @@
 #include <SPI.h>
 #include <SD.h>
+
 const int chipSelect = 10;
 File myFile;
+
 void setup() {
-Serial.begin(9600);
-while (!Serial) { ; }
-if (!SD.begin(chipSelect)) {
-Serial.println("SD initialization failed!");
-while (1);
+  Serial.begin(9600);
+  while (!Serial) { ; }
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD initialization failed!");
+    while (1);
+  }
+
+  myFile = SD.open("data.csv");
+  if (myFile) {
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    myFile.close();
+  } else {
+    Serial.println("Error: Could not open data.csv");
+  }
 }
-myFile = SD.open("data.csv");
-if (myFile) {
-while (myFile.available()) {
-Serial.write(myFile.read());
-}
-myFile.close();
-} else {
-Serial.println("Error: Could not open data.csv");
-}
-}
+
 void loop() {
 }
